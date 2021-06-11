e8606ee9090e6aa511612e65604b5d0ae25548fa
Cornéll Myburgh <cornellmyburgh@outlook.com>
2021-06-11 10:17:53 +0200

    Everything running fine, waiting to be marked

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 function getInputValueTask(){
-            // Selecting the input element and get its value 
-           let listVal = document.getElementById("listTask").value;
-           let listDate = document.getElementById("dueDate").value;
+    // Selecting the input element and get its value 
+    let listVal = document.getElementById("listTask").value;
+    
+    let listDate = document.getElementById("dueDate").value;
+  
+    let obj = document.getElementById("listTask").value;
+    
+    let obj1 = document.getElementById("dueDate").value;
+    
+    const myData = JSON.stringify(obj);
+    
+    const myDate = JSON.stringify(obj1)
+    
+    localStorage.setItem("userData", myData);
+    localStorage.setItem("userDate", myDate);
 
-            // Displaying the value
-           console.log(listVal); 
-                       console.log(listDate);   
-
-
-            let obj = document.getElementById("listTask").value;
-            let obj1 = document.getElementById("dueDate").value;
-            const myData = JSON.stringify(obj);
-            const myDate = JSON.stringify(obj1)
-            localStorage.setItem("userData", myData);
-            localStorage.setItem("userDate", myDate);
-
-            myText = localStorage.getItem("userData, userDate");
-            newObj = JSON.parse(myText);
+    myText = localStorage.getItem("userData, userDate");
+    newObj = JSON.parse(myText);
 
 }
 let newList = document.getElementById("my_span1")
+
 function addTask() {
     let Task = document.getElementById("listTask").value;
     let entry = document.createElement("li");
@@ -34,42 +35,36 @@ function addDate() {
     newDate.appendChild(date);
 }
 let getItem = localStorage.getItem("userData");
+
 let getItem1 = localStorage.getItem("userDate");
+
 my_span1.appendChild(document.createTextNode(getItem));
 my_span1.appendChild(document.createTextNode(getItem1));
 
 
  function sortAbc() {
-    // Declaring Variables
-            let sortList, i, run, li, stop;
-  
-            // Taking content of list as input
-            sortList = document.getElementById("my_span1");
-  
-            run = true;
-  
-            while (run) {
-                run = false;
-                li = sortList.getElementsByTagName("LI");
-  
-                // Loop traversing through all the list items
-                for (i = 0; i < (li.length - 1); i++) {
-                    stop = false;
-                    if (li[i].innerHTML.toLowerCase() > 
-                        li[i + 1].innerHTML.toLowerCase()) {
-                        stop = true;
-                        break;
+    let sortList, i, run, li, stop;
+     
+    sortList = document.getElementById("my_span1");
+    run = true;
+
+    while (run) {
+        run = false;
+        li = sortList.getElementsByTagName("LI");
+
+    for (i = 0; i < (li.length - 1); i++) {
+        stop = false;
+        if (li[i].innerHTML.toLowerCase() > 
+        li[i + 1].innerHTML.toLowerCase()) {
+        stop = true;
+        break;
 }
 }
-  
-                /* If the current item is smaller than 
-                   the next item then adding it after 
-                   it using insertBefore() method */
-                if (stop) {
-                    li[i].parentNode.insertBefore(
-                            li[i + 1], li[i]);
-  
-                    run = true;
+    if (stop) {
+        li[i].parentNode.insertBefore(
+        li[i + 1], li[i]);
+
+        run = true;
 }
 }
 }
@@ -90,22 +85,22 @@ const formatTextWrap = (text, maxLineLength) => {
   return output
 }
 function my_fun(j){
+    let chkbox ="ckb" + j;
 
-let chkbox ="ckb" + j;
+    let my_span ="my_span" + j;
 
-let my_span ="my_span" + j;
-
-let msg = chkbox + "  " + my_span;
+    let msg = chkbox + "  " + my_span;
 
 if(document.getElementById(chkbox).checked){ 
 
-document.getElementById(my_span).style.textDecoration='line-through';
+    document.getElementById(my_span).style.textDecoration='line-through';
 
 }
 else{
-document.getElementById(my_span).style.textDecoration='none';
+    document.getElementById(my_span).style.textDecoration='none';
 }
 }
 function deleteButton(){
  localStorage.removeItem("userData");
+ localStorage.removeItem("userDate");
 }
